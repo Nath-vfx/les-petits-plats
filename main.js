11bdff6d7b177aa60a92ee654cb2cc636644b021
Nathan Van Bignoot <70898448+Nath-vfx@users.noreply.github.com>
2024-09-26 16:45:16 +0200

    feat: display all filters

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
-import { recipes } from "./recipes.js"
-const menu = document.querySelector(".nav-test")
+import {recipes} from "./recipes.js"
+
+// const menu = document.querySelector(".nav-test")
 
 let recipesList = recipes
 
-function toggleNavBar() {
-    menu.classList.toggle("open")
+// function toggleNavBar() {
+//     menu.classList.toggle("open")
+// }
+
+async function getAllIngredients() {
+    const ingredients = [];
+    recipesList.forEach((recipe) => {
+        recipe.ingredients.forEach((ingredient) => {
+            ingredients.push(ingredient.ingredient);
+        });
+    });
+    return ingredients;
 }
+
+async function getAllUstensils() {
+    const ustensils = [];
+    recipesList.forEach((recipe) => {
+        recipe.ustensils.forEach((ustensil) => {
+            ustensils.push(ustensil);
+        });
+    });
+    return ustensils;
+}
+
+async function getAllAppliances() {
+    const appliances = [];
+    recipesList.forEach((recipe) => {
+        appliances.push(recipe.appliance);
+    });
+    return appliances;
+}
+
+
+async function displayStuffs(stuffs, name) {
+    const e = [...new Set(stuffs)].sort((a, b) => a.localeCompare(b));
+
+    const stuffsContainer = document.querySelector(`.${name} ul`);
+    let allStuffs = "";
+
+    e.forEach((stuff) => {
+        allStuffs += `
+        <li class="">
+            <a>${stuff}</a>
+        </li>
+        `
+    })
+
+    stuffsContainer.innerHTML = allStuffs;
+}
+
 
 async function displayRecipes() {
     const recipesContainer = document.querySelector(".recipes-container")
@@ -32,9 +80,9 @@ async function displayRecipes() {
                     </h4>
                     <ul class="recipe-ingredients-list">
                         <li><p>Ingredient</p><span>Quantity</span></li>
-                        ${recipe.ingredients.map((ingredient) => 
-                            `<li><p>${ingredient.ingredient}</p><span>${ingredient.quantity}</span></li>`
-                            ).join("")}
+                        ${recipe.ingredients.map((ingredient) =>
+            `<li><p>${ingredient.ingredient}</p><span>${ingredient.quantity}</span></li>`
+        ).join("")}
                     </ul>
                 </div>
             </div>
@@ -47,6 +95,9 @@ async function displayRecipes() {
 
 async function init() {
     await displayRecipes();
+    await displayStuffs(await getAllIngredients(), "ingredients");
+    await displayStuffs(await getAllUstensils(), "ustensils");
+    await displayStuffs(await getAllAppliances(), "appliances");
 }
 
 init();
